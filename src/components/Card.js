b3a92fdab502fa6e5cfb7d9c04f356587fb35f5c
OmverYo <64951757+OmverYo@users.noreply.github.com>
2023-02-07 10:14:46 +1300

    Text and Image formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,12 @@ export default function Card(props) {
     return (
         <div className="card">
             <img src={props.item.imageUrl} className="card--image" alt="TravelImage"></img>
-            <p>{props.item.location} <a href={props.item.googleMapsUrl}>View on Google Maps</a></p>
-            <h3>{props.item.title}</h3>
-            <h3>{props.item.startDate} - {props.item.endDate}</h3>
-            <p>{props.item.description}</p>
+            <div className="card--text">
+                <p>{props.item.location} <a href={props.item.googleMapsUrl}>View on Google Maps</a></p>
+                <h3>{props.item.title}</h3>
+                <h3>{props.item.startDate} - {props.item.endDate}</h3>
+                <p>{props.item.description}</p>
+            </div>
             <div className="card--borderbottom"></div>
         </div>
     )
