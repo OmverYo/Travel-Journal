cfdbeccf7d520a32c449686e24106900849958dd
OmverYo <64951757+OmverYo@users.noreply.github.com>
2023-02-07 13:31:50 +1300

    Added gps icon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,26 @@
 import React from "react"
+import GPS from "./../images/gps-icon.png"
+import data from "./../data"
 
 export default function Card(props) {
+    let max = "Not Max"
+
+    if (props.item.id === data.length) {
+        max = "MAX"
+    } else {
+        max = "Not Max"
+    }
+
     return (
         <div className="card">
             <img src={props.item.imageUrl} className="card--image" alt="TravelImage"></img>
             <div className="card--text">
-                <p>{props.item.location} <a href={props.item.googleMapsUrl}>View on Google Maps</a></p>
+                <p><img src={GPS} className="card--gpsicon" alt="GPS-Icon"></img>{props.item.location} <a href={props.item.googleMapsUrl} className="gray">View on Google Maps</a></p>
                 <h3>{props.item.title}</h3>
                 <h3>{props.item.startDate} - {props.item.endDate}</h3>
                 <p>{props.item.description}</p>
             </div>
-            <div className="card--borderbottom"></div>
+            {max = "Not Max" && <div className="card--borderbottom"></div>}
         </div>
     )
 }
