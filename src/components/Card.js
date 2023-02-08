ba48167bae35b25d498ba0d47f266be1af7f77ab
OmverYo <justinlee990106@gmail.com>
2023-02-08 19:31:36 +1300

    Fixing some errors

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,11 +16,11 @@ export default function Card(props) {
             <img src={props.item.imageUrl} className="card--image" alt="TravelImage"></img>
             <div className="card--text">
                 <p><img src={GPS} className="card--gpsicon" alt="GPS-Icon"></img>{props.item.location} <a href={props.item.googleMapsUrl} className="gray">View on Google Maps</a></p>
-                <h3>{props.item.title}</h3>
-                <h3>{props.item.startDate} - {props.item.endDate}</h3>
+                <h1 className="card--title">{props.item.title}</h1>
+                <p className="card--date"><b>{props.item.startDate} - {props.item.endDate}</b></p>
                 <p>{props.item.description}</p>
             </div>
-            {max = "Not Max" && <div className="card--borderbottom"></div>}
+            {max === "Not Max" && <div className="card--borderbottom"></div>}
         </div>
     )
 }
